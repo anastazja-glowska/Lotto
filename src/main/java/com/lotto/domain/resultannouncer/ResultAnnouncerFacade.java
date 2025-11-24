ba55de515bb816f1e068c0aeec855e1062e5bec7
Anastazja Glowska <anastazjaglowska12345@gmail.com>
2025-11-24 20:35:39 +0100

    Implementing redis configuration

--- src/main/java/com/lotto/domain/resultannouncer/ResultAnnouncerFacade.java
+++ src/main/java/com/lotto/domain/resultannouncer/ResultAnnouncerFacade.java
@@ -3,12 +3,14 @@ package com.lotto.domain.resultannouncer;
 
 import com.lotto.domain.resultannouncer.dto.ResultMessageDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 
 @RequiredArgsConstructor
 public class ResultAnnouncerFacade {
 
     private final ResultAnnounceRetriever resultAnnounceRetriever;
 
+    @Cacheable("lotto")
     public ResultMessageDto checkPlayResultByHash(String hash) {
         return resultAnnounceRetriever.checkPlayResultByHash(hash);
     }
--- src/main/java/com/lotto/domain/resultannouncer/dto/ResultMessageDto.java
+++ src/main/java/com/lotto/domain/resultannouncer/dto/ResultMessageDto.java
@@ -2,6 +2,8 @@ package com.lotto.domain.resultannouncer.dto;
 
 import lombok.Builder;
 
+import java.io.Serializable;
+
 @Builder
-public record ResultMessageDto(String message, ResultDto resultDto) {
+public record ResultMessageDto(String message, ResultDto resultDto) implements Serializable {
 }
